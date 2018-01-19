6afc8124df97e4f1e3e27bd3fffb87c9672c062d
Suilong Liang <suilong.liang@moxi.xyz>
2018-01-19 16:09:54 +0800

    remove useless comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,15 @@ if [[ $1 == 'start' ]]; then
         sed 's/daemonize yes/daemonize no/g' -i $REDIS_CONF
         sed 's/bind 127.0.0.1/bind 0.0.0./g' -i $REDIS_CONF
 	sed 's/appendonly no/appendonly yes/g' -i $REDIS_CONF
+
 	for file in `ls /etc/redis/conf.d/`;
 	do echo "include /etc/redis/conf.d/$file" >> $REDIS_CONF;
 	done
 	
-        echo "aaa"
 	if [[ -s /var/lib/redis/appendonly.aof ]]; then
-		echo "ccc"
 		echo "y" | redis-check-aof --fix /var/lib/redis/appendonly.aof
-		echo "ddd"
 	fi
-        echo "bbb"
+
         exec gosu redis /usr/bin/redis-server $REDIS_CONF
 fi
 
